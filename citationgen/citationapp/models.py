b9fd448662a4d29ce1283372847cba72cab904a5
Saskia <saskia.tjioe@gmail.com>
2019-01-20 12:01:56 -0500

    Added a new model and configured urls

--- citationgen/citationapp/models.py
+++ citationgen/citationapp/models.py
@@ -1,3 +1,15 @@
+import datetime
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+
+class CiteBookField(models.Model):
+    book_cite_text = models.CharField(max_length=200)
+    cite_date = models.DateTimeField('citation made:')
+
+    def __str__(self):
+        return self.book_cite_text
+
+    def was_citation_recent(self):
+        return self.cite_date >= timezone.now() - datetime.timedelta(days=1)
--- citationgen/citationapp/views.py
+++ citationgen/citationapp/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.template import loader
+from .models import CiteBookField
 # Create your views here.
+
+
+def index(request):
+    citation_history = CiteBookField.objects.order_by('-cite_date')[:5]
+    template = loader.get_template('citationapp/index.html')
+    context = {
+        'citation_history': citation_history,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+'''
+The results function below handles the view for the results of the
+provided inputs. It displays the complete citation
+'''
+
+
+def results(request, citation_id):
+    response = "Here is your citation for %s"
+    return HttpResponse(response % citation_id)
