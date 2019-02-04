cc30b2f7418ca63fd460a55ace514dce2d265118
Saskia <saskia.tjioe@gmail.com>
2019-02-03 21:06:01 -0500

    Added HTML templates home and history and added extra views in citationapp

--- citationgen/citationapp/urls.py
+++ citationgen/citationapp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.home, name="home"),
+    path('forms/', views.fill_form, name="forms"),
+    path('forms/results/', views.results, name="results"),
+    path('forms/results/history/', views.history, name="history"),
 ]
--- citationgen/citationapp/views.py
+++ citationgen/citationapp/views.py
@@ -4,10 +4,25 @@ from django.template import loader
 from .models import CiteBookField
 # Create your views here.
 
+'''
+home function: generates the home page view of the citation generator
+'''
 
-def index(request):
+
+def home():
+    template = loader.get_template('citationapp/home.html')
+    return template
+
+
+'''
+history function displays past citations made by the user. Is displayed
+after the form is submitted.
+'''
+
+
+def history(request):
     citation_history = CiteBookField.objects.order_by('-cite_date')[:5]
-    template = loader.get_template('citationapp/index.html')
+    template = loader.get_template('citationapp/history.html')
     context = {
         'citation_history': citation_history,
     }
@@ -16,10 +31,20 @@ def index(request):
 
 '''
 The results function below handles the view for the results of the
-provided inputs. It displays the complete citation
+provided inputs. It displays the complete citation.
 '''
 
 
 def results(request, citation_id):
     response = "Here is your citation for %s"
     return HttpResponse(response % citation_id)
+
+
+'''
+The fill_form function displays the fields that the user must fill in order to
+generate the citation. For now this form will take the inputs for book citations. 
+'''
+
+
+def fill_form(request, citation_id):
+    return HttpResponse("Fill out as many blanks as you can to generate your citation.")
